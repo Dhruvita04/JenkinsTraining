0d0d8bc7be363b86cdab73c7ce2630a2ae639a16
Dhruv Mehta <dhruvashmm1@gmail.com>
2026-07-21 13:02:03 +0530

    Jenkins12

---
+++ src/main/java/com/example/jenkins/models/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.jenkins.models;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/")
+@RestController
+public class HelloController {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+
+}
